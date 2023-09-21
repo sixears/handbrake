919143c0805454139e31e1bede8d5207bd587409
Mr. Fluffy <git@sixearsm>
2023-09-21 13:23:55 +0000

    remove src/HandBrake/Paths.hs from git

--- src/HandBrake/Paths.hs
+++
@@ -1,6 +0,0 @@
-module HandBrake.Paths where
-
-import FPath.AbsFile  ( AbsFile, absfile )
-
-handbrakeCLI :: AbsFile
-handbrakeCLI = [absfile|/nix/store/zmn279g2alk107bmsyc3cdvxk6wrr9p5-handbrake-1.3.3/bin/HandBrakeCLI|]
